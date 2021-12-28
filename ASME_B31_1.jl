6982ed8bc0695d3358a45ef29ec684a72dae551b
Jia Chen <jiajiamomomo@gmail.com>
2021-12-29 01:13:32 +0800

    Update ASME_B31_1.jl

--- ASME_B31_1.jl
+++ ASME_B31_1.jl
@@ -1,4 +1,14 @@
-function minwallthk(P::T, Do::T, SE::T, W::T, y::T, A::T)::T where {T<:AbstractFloat}
+"""
+    minwallthkDo(P, Do, SE, W, y, A)
+Minimum Wall Thickness per ASME B31.1 paras. 104.1.2 eq.(7), in. (mm)
+P: internal design pressure, psig [kPa (gage)]
+Do: outside diameter of pipe, in. (mm)
+SE: maximum allowable stress in material, psi (MPa)
+W: weld strength reduction factor
+y: coefficient in Table 104.1.2-1
+A: additional thickness, in. (mm)
+"""
+function minwallthkDo(P::T, Do::T, SE::T, W::T, y::T, A::T)::T where {T<:AbstractFloat}
 return P * Do / 2 / (SE * W + P * y) + A
 end
 minwallthk(P::AbstractFloat, Do::AbstractFloat, SE::AbstractFloat, W::AbstractFloat, y::AbstractFloat, A::AbstractFloat) = minwallthk(promote(P, Do, SE, W, y, A)...)
